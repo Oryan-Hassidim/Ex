6aa5ce711b09369d7d8a72622dcc2ed25386cc50
Oryan.Hassidim <oryan.hassidim@gmail.com>
2022-03-08 12:23:01 +0200

    format code

--- Ex1/hello_turtle.py
+++ Ex1/hello_turtle.py
@@ -1,8 +1,8 @@
 #############################################################
-# FILE : Ex.py 
-# WRITER : Oryan Hassidim , oryan.hassidim , 319131579  
-# EXERCISE : intro2cs2 Ex1 2022 
-# DESCRIPTION: A simple Program that draws flowers with 
+# FILE : Ex.py
+# WRITER : Oryan Hassidim , oryan.hassidim , 319131579
+# EXERCISE : intro2cs2 Ex1 2022
+# DESCRIPTION: A simple Program that draws flowers with
 # python turtle like a LOGO app.
 # STUDENTS I DISCUSSED THE EXERCISE WITH: --
 # WEB PAGES I USED: https://docs.python.org/3/library/turtle.html
@@ -12,10 +12,11 @@
 
 from turtle import *
 
+
 def draw_petal():
     """Draws single petal on the screen.
     Starts from current point of the turtle."""
-    for i in [1,2]:
+    for i in [1, 2]:
         fd(30)
         rt(45)
         fd(30)
@@ -32,6 +33,7 @@ def draw_flower():
     lt(135-90)
     fd(150)
 
+
 def draw_flower_and_advance():
     """Draws single flower on the screen and advances
     the turtle to the position of the next flower.
@@ -45,6 +47,7 @@ def draw_flower_and_advance():
     lt(90)
     pd()
 
+
 def draw_flower_bed():
     """Draws 3 flowers on the screen.
     Starts from (0,0)."""
@@ -54,6 +57,7 @@ def draw_flower_bed():
     pd()
     for i in range(3):
         draw_flower_and_advance()
+
 
 if __name__ == "__main__":
     draw_flower_bed()
--- Ex1/math_print.py
+++ Ex1/math_print.py
@@ -1,5 +1,4 @@
-﻿
-# FILE : Ex.py
+﻿# FILE : Ex.py
 # WRITER : Oryan Hassidim , oryan.hassidim , 319131579
 # EXERCISE : intro2cs2 Ex1 2022
 # DESCRIPTION: A simple Program that prints to the console
@@ -19,28 +18,34 @@ def golden_ratio():
     https://en.wikipedia.org/wiki/Golden_ratio"""
     print((1 + sqrt(5)) / 2)
 
+
 def six_squared():
     """Prints the evaluation of 6 square (6²)."""
-    print(pow(6,2))
+    print(pow(6, 2))
+
 
 def hypotenuse():
     """Prints te length of excess of right-angled
     triangle with sides length: 12, 5."""
     print(hypot(12, 5))
 
+
 def pi():
     """Prints the constant value Pi (π)."""
     print(math.pi)
+
 
 def e():
     """Prints the constant value e."""
     print(math.e)
 
+
 def squares_area():
     """Prints the area of squares with side lenght betwwen
     1 to 10."""
-    squares = [i**2 for i in range(1,11)]
+    squares = [i**2 for i in range(1, 11)]
     print(*squares)
+
 
 if __name__ == "__main__":
     golden_ratio()
