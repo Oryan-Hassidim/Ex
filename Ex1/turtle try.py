c0ffd84d72aff503c0d0ab662df5da59c993d30e
Oryan.Hassidim <oryan.hassidim@gmail.com>
2022-03-15 01:33:12 +0200

    Ex2

--- Ex1/turtle try.py
+++ Ex1/turtle try.py
@@ -1,44 +1,69 @@
 #############################################################
-# FILE : Ex.py 
-# WRITER : Oryan Hassidim , oryan.hassidim , 319131579  
-# EXERCISE : intro2cs2 Ex1 2022 
-# DESCRIPTION: A simple program that prints "Hello World!" to  
-# the standard output (screen). 
+# FILE : Ex.py
+# WRITER : Oryan Hassidim , oryan.hassidim , 319131579
+# EXERCISE : intro2cs2 Ex1 2022
+# DESCRIPTION: A simple program that prints "Hello World!" to
+# the standard output (screen).
 #############################################################
 
-from turtle import *
+# from turtle import *
 
-big = 200
-step = 20
+# big = 200
+# step = 20
 
-clear()
+# clear()
 
-speed(5000)
+# speed(5000)
 
-pu()
-bk(big / 2)
-rt(90)
-bk(big / 2)
-lt(90)
-pd()
+# pu()
+# bk(big / 2)
+# rt(90)
+# bk(big / 2)
+# lt(90)
+# pd()
 
-for j in [1,2,3,4,5,6]:
-    print(j)
-    for s in range(big, 0, -step):
-        for i in range(1,5):
-            forward(s)
-            right(90)
-        pu()
-        fd(step/2)
-        rt(90)
-        fd(step/2)
-        lt(90)
-        pd()
+# for j in [1,2,3,4,5,6]:
+#    print(j)
+#    for s in range(big, 0, -step):
+#        for i in range(1,5):
+#            forward(s)
+#            right(90)
+#        pu()
+#        fd(step/2)
+#        rt(90)
+#        fd(step/2)
+#        lt(90)
+#        pd()
 
-    pu()
-    rt(15)
-    bk(big / 2)
-    rt(90)
-    bk(big / 2)
-    lt(90)
-    pd()
+#    pu()
+#    rt(15)
+#    bk(big / 2)
+#    rt(90)
+#    bk(big / 2)
+#    lt(90)
+#    pd()
+
+
+import array as arr
+from array import *
+
+lst = [300, 301, 302, 303]
+for x in lst:
+    print(x)
+    print(id(x))
+
+arr1 = array("I", (x for x in range(10)))
+bs = array.tobytes(arr1)
+print(bs)
+for x in arr1:
+    print(x)
+    print(id(x))
+
+from pipe import select, where
+
+arr = [1, 2, 3, 4, 5]
+lst = list(arr
+     | where (lambda x: x % 2 == 0)
+     | select (lambda x: x *2))
+
+print(lst)
---
+++ Ex2/largest_and_smallest.py
@@ -0,0 +1,45 @@
+#############################################################
+# FILE : largest_and_smallest.py
+# WRITER : Oryan Hassidim , oryan.hassidim , 319131579
+# EXERCISE : intro2cs2 Ex2 2022
+# DESCRIPTION: Function to get the largest and smallest
+#              number from 3 numbers, and chek this function.
+# STUDENTS I DISCUSSED THE EXERCISE WITH: --
+# WEB PAGES I USED: --
+# NOTES: 0,0.0,-0.0 => check about same falue in different
+#                      types and forms.
+#        0,float('0.'+'0'*100+'1'),float('-0.'+'0'*100+'1')
+#        => chek very small change.
+#############################################################
+
+# 3
+def largest_and_smallest(a, b, c):
+    """Gets 3 numbers, and return tuple of two numbers 
+    - the largest one (first) and the smallest one (second)"""
+    l = (a, b, c)
+    small, large = a, a
+    for x in l:
+        if x > large:
+            large = x
+        elif x < small:
+            small = x
+    return large, small
+
+
+# 8
+def check_largest_and_smallest():
+    """Checks the largest_and_smallest function on some
+    inputs"""
+    if largest_and_smallest(17, 1, 6) != (17, 1):
+        return False
+    if largest_and_smallest(1, 17, 6) != (17, 1):
+        return False
+    if largest_and_smallest(1, 1, 2) != (2, 1):
+        return False
+    if largest_and_smallest(0, 0.0, -0.0) != (0, 0):
+        return False
+    if largest_and_smallest(
+        0, float("0." + "0" * 100 + "1"), float("-0." + "0" * 100 + "1")
+    ) != (10 ** -101, -(10 ** -101)):
+        return False
+    return True
