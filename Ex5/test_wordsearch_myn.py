3b395dd310b84141b2eee8e66101b03ba202cd24
Oryan Hassidim <oryan.hassidim@gmail.com>
2022-04-10 23:59:59 +0300

    tests

--- Ex5/test_wordsearch_myn.py
+++ Ex5/test_wordsearch_myn.py
@@ -1,7 +1,5 @@
 import os
 import sys
-import tempfile
-import inspect
 
 import wordsearch
 from wordsearch import *
@@ -39,7 +37,7 @@ def test_5():
 def test_6():
     directions = ["d", "l", "r", "u", "w", "x", "y", "z", "udlrwxyz"]
     base_path = "C:\\Projects\\Oryan-Hassidim\\Ex\\Ex5\\Additional Files\\"
-    while len(argv) < 5:
+    for _ in range(4):
         argv.insert(1, "")
     for d in directions:
         argv[1] = base_path + "word_list.txt"
@@ -50,4 +48,46 @@ def test_6():
         assert os.path.isfile("C:\\users\\oryan\\desktop\\output.txt")
         expected = read_wordlist(f"{base_path}outfile_{d}.txt")
         actual = read_wordlist("C:\\users\\oryan\\desktop\\output.txt")
+        assert len(actual) == len(set(actual))
         assert set(expected) == set(actual)
+    for _ in range(4):
+        del(argv[1])
+
+
+
+def test_7():
+    assert find_words(["bob"], [["b", "o", "b"]], "lr") == [("bob", 2)]
+
+
+def test_8():
+    assert find_words(["bob"], [["b", "O", "b"]], "lr") == []
+
+
+def test_9():
+    expected = [("dog", 1), ("god", 1)]
+    actual = find_words(["dog", "god"], [["d", "o", "g"]], "lr")
+    assert len(actual) == len(set(actual))
+    assert set(expected) == set(actual)
+
+
+def test_10():
+    expected = [("bob", 2)]
+    actual = find_words(["bob"], [["b"], ["o"], ["b"], ["o"], ["b"]], "d")
+    assert len(actual) == len(set(actual))
+    assert set(expected) == set(actual)
+
+
+def test_11():
+    expected = [("bob", 4)]
+    actual = find_words(["bob"], [["b"], ["o"], ["b"], ["o"], ["b"]], "ud")
+    assert len(actual) == len(set(actual))
+    assert set(expected) == set(actual)
+
+
+def test_11():
+    expected = [("bob", 2), ("cat", 1), ("bobcat", 1)]
+    actual = find_words(
+        ["bob", "cat", "bobcat"], [["b", "o", "b", "c", "a", "t"]], "lr"
+    )
+    assert len(actual) == len(set(actual))
+    assert set(expected) == set(actual)
